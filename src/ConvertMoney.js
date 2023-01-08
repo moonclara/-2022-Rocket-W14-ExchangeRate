cb044a54f4a1bd354a339e21f998fccbae9b8934
Clara <smile629639@gmail.com>
2023-01-08 11:16:09 +0800

    安裝tailwind並加上樣式

--- src/ConvertMoney.js
+++ src/ConvertMoney.js
@@ -69,6 +69,7 @@ function ConvertMoney() {
       <li key={i}>
         {item.name}：{Math.round(item.rate * convert)}元
         <input
+          className="bg-stone-500 text-white px-2 py-1 ml-2 rounded hover:bg-stone-700 cursor-pointer"
           type="button"
           value="兌換"
           onClick={() => {
@@ -103,38 +104,55 @@ function ConvertMoney() {
 
   return (
     <>
-      <h3>新增幣種</h3>
-      <input
-        type="text"
-        placeholder="幣種名稱"
-        name="name"
-        value={newRate.name}
-        onChange={handleChangeNewRate}
-      />
-      <input
-        type="text"
-        placeholder="匯率"
-        name="rate"
-        value={newRate.rate}
-        onChange={handleChangeNewRate}
-      />
-      <input type="button" value="新增幣種" onClick={handleClickNewRate} />
-      <hr />
-      <h3>您的錢包還有{wallet}元</h3>
-      <label htmlFor="inputNTD">請輸入您要換的台幣</label>
-      <input
-        id="inputNTD"
-        type="number"
-        placeholder="台幣"
-        value={inputMoney}
-        onChange={handleChangeInputMoney}
-      />
-      <input type="button" value="計算" onClick={handleClickInputMoney} />
-      <p>可以換算</p>
-      <ul>{handleExchangeRates}</ul>
-      <hr />
-      <h3>您的兌換紀錄</h3>
-      <ul>{handleExchangeRecord}</ul>
+      <div className="bg-white w-[1024px] rounded-lg px-10 py-5 absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%]">
+        <h3>新增幣種</h3>
+        <input
+          className="border border-neutral-200 focus:outline-none focus:border-stone-200 focus:ring-stone-200 focus:ring-1 rounded p-1 mr-2 mb-5"
+          type="text"
+          placeholder="幣種名稱"
+          name="name"
+          value={newRate.name}
+          onChange={handleChangeNewRate}
+        />
+        <input
+          className="border border-neutral-200 focus:outline-none focus:border-stone-200 focus:ring-stone-200 focus:ring-1 rounded p-1 mr-2"
+          type="text"
+          placeholder="匯率"
+          name="rate"
+          value={newRate.rate}
+          onChange={handleChangeNewRate}
+        />
+        <input
+          className="bg-stone-500 text-white px-2 py-1 rounded hover:bg-stone-700 cursor-pointer"
+          type="button"
+          value="新增幣種"
+          onClick={handleClickNewRate}
+        />
+        <hr />
+        <h3>您的錢包還有{wallet}元</h3>
+        <label htmlFor="inputNTD" className="mr-4">
+          請輸入您要換的台幣
+        </label>
+        <input
+          className="border border-neutral-200 focus:outline-none focus:border-stone-200 focus:ring-stone-200 focus:ring-1 rounded p-1 mr-2 mb-5"
+          id="inputNTD"
+          type="number"
+          placeholder="台幣"
+          value={inputMoney}
+          onChange={handleChangeInputMoney}
+        />
+        <input
+          className="bg-stone-500 text-white px-2 py-1 rounded hover:bg-stone-700 cursor-pointer"
+          type="button"
+          value="計算"
+          onClick={handleClickInputMoney}
+        />
+        <p className="mb-5">可以換算</p>
+        <ul>{handleExchangeRates}</ul>
+        <hr />
+        <h3>您的兌換紀錄</h3>
+        <ul>{handleExchangeRecord}</ul>
+      </div>
     </>
   );
 }
